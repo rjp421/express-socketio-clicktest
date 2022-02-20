458ca1de327f92be1be6d7b17c2fae23e2c74fc1
rjp421 <rjp421@gmail.com>
2022-02-20 12:43:07 -0800

    test undo

--- index.js
+++ index.js
@@ -47,16 +47,16 @@ io.on('connection', onIOConnect);
 function onIOConnect(socket) {
     socket.uuid = UUID.v4();
 
-    console.log(`Client connected:  ${socket.handshake.address}  sid:${socket.id}  uuid:${socket.uuid}`);
+    console.log(`Client connected:  ${socket.handshake.address}  sid:${socket.id}  uuid:${socket.uuid}  url: ${socket.handshake.url}`);
     
-    socket.on('click', onClientClick);    
+    socket.on('click', onClientClick);
     socket.on('retrieve', onRetrieveClicks);
     socket.on('clearItems', onClearItems);
+    socket.on('undoItem', onUndoItem);
 
     socket.on('disconnect', function onSocketDisconnect(reason) {
         console.log(`Client disconnected:  ${socket.handshake.address}  sid:${socket.id}  uuid:${socket.uuid} - ${reason}`);
         delete connectedUsersObj[socket.uuid];
-        //delete socket.uuid;
     });
 
     socket.emit('clientConnected', socket.uuid);
@@ -73,14 +73,12 @@ function onClientClick(msgObj) {
 
     connectedUsersObj[clientId].broadcast.emit('click', {x, y, radius, color});
     
-    console.log(`Client ${connectedUsersObj[clientId].handshake.address}/${clientId} clicked ${x},${y}.`);
+    console.log(`Client ${connectedUsersObj[clientId].handshake.address}/${clientId} clicked ${x}, ${y}, ${radius}, ${color}, new points.length: ${points.length}`);
 }
 
 
 function onRetrieveClicks(clientId) {
     if (!connectedUsersObj[clientId]) return;
-
-    console.log(points);
 
     connectedUsersObj[clientId].emit('clicks', points);
 }
@@ -88,7 +86,7 @@ function onRetrieveClicks(clientId) {
 
 function onClearItems(clientId) {
     if (!connectedUsersObj[clientId]) return;
-
+    
     // empty the points Array
     while (points.length) points.pop();
 
@@ -97,6 +95,19 @@ function onClearItems(clientId) {
     // tell the other clients to clear items
     connectedUsersObj[clientId].broadcast.emit('clearItems');
 }
+
+
+function onUndoItem(clientId) {
+    if (!connectedUsersObj[clientId]) return;
+    
+    if (points.length) points.pop();
+    
+    console.log(`Client ${connectedUsersObj[clientId].handshake.address}/${clientId} popped the points Array, new length: ${points.length}`);
+    
+    // tell the other clients to undo the last item
+    connectedUsersObj[clientId].broadcast.emit('undoItem');
+}
+
 
 
 
